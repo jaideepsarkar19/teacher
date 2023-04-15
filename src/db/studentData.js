109539ce1f8ed1694360d2cb5f452844d272c389
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-04-15 17:33:58 +0530

    1st

---
+++ src/db/studentData.js
@@ -0,0 +1,17 @@
+export const intialState = {
+students:[
+    {   name: "Shama Parekh",   gender: "Male",   class: 12,   birthdate: "7-12-2005",   address: "1748 Nerpi View",   street: "Jusu Lane",   city: "Govdante",   state: "AK",   country: "SM",   zip: 67497,   email:"difahoj@acibaho.md",   teacher: "RAJIV DAS" },
+    {   name: "Isaac Bowen",   gender: "Female",   class: 11,   birthdate: "5-9-2006",   address: "762 Migof Road",   street: "Zatena Way",   city: "Ucudawas",   state: "TX",   country: "FJ",   zip: 26901,   email:"uzuocagu@wor.cf",   teacher: "ANNA PAUL" },
+    {   name: "Steven Thomas",   gender: "Male",   class: 11,   birthdate: "5-27-2005",   address: "154 Buha Extension",   street: "Gunul Glen",   city: "Ejahisira",   state: "MD",   country: "MQ",   zip: 58254,   email:"za@wu.co.uk",   teacher: "SAM SINGH" },
+    {   name: "Tony Floyd",   gender: "Female",   class: 12,   birthdate: "10-5-2006",   address: "1755 Vegec Center",   street: "Buhled Grove",   city: "Pibdeed",   state: "NV",   country: "ET",   zip: 76260,   email:"ijluge@wol.tf",   teacher: "RAJ SINGH" },
+    {   name: "Maurice Casey",   gender: "Female",   class: 12,   birthdate: "7-12-2004",   address: "1029 Cuzgim Junction",   street: "Uwopo Parkway",   city: "Pogkoeco",   state: "NC",   country: "BQ",   zip: 63342,   email:"povgeluk@lu.ar",   teacher: "DHARM SINGH" },
+    {   name: "ekta bhosale",   gender: "Female",   class: 11,   birthdate: "5-19-2008",   address: "701 Dowcu View",   street: "Ipec Parkway",   city: "Ciwmofus",   state: "VA",   country: "EE",   zip: 96143,   email:"okde@tutifo.nc",   teacher: "AMIT GHOSH" },
+    {   name: "Dollie Lowe",   gender: "Female",   class: 11,   birthdate: "9-29-2006",   address: "814 Wicej Plaza",   street: "Zukip Junction",   city: "Kimnute",   state: "KY",   country: "CY",   zip: 10029,   email:"maikude@usujorak.is",   teacher: "RAJIV DAS" },
+    {   name: "Stella Rivera",   gender: "Male",   class: 11,   birthdate: "9-16-2007",   address: "939 Lihpac Manor",   street: "Vopem View",   city: "Cisnutij",   state: "WV",   country: "LV",   zip: 43646,   email:"ni@opefuti.dj",   teacher: "ANNA PAUL" },
+    {   name: "Genevieve Craig",   gender: "Male",   class: 11,   birthdate: "7-22-2005",   address: "1266 Zawna Junction",   street: "Jibba Key",   city: "Padkehrog",   state: "SC",   country: "CF",   zip: 17466,   email:"wa@wi.jm",   teacher: "SAM SINGH" },
+    {   name: "Mildred Peterson",   gender: "Female",   class: 12,   birthdate: "7-12-2003",   address: "1060 Sail Trail",   street: "Canno Terrace",   city: "Jovkotof",   state: "IN",   country: "LA",   zip: 85616,   email:"uwice@radisof.fr",   teacher: "DHARM SINGH" },
+    {   name: "Maria Yates",   gender: "Female",   class:12,   birthdate: "8-20-2003",   address: "442 Sohaga Square",   street: "Betluj Turnpike",   city: "Filbeig",   state: "AK",   country: "KW",   zip: 76622,   email:"gewac@nizpol.st",  teacher: "AMIT GHOSH" },
+    {   name: "Lula Miles",   gender: "Female",   class: 12,   birthdate: "2-24-2006",   address: "267 Pahpac River",   street: "Puhwir Avenue",   city: "Howotpu",   state: "OH",   country: "OM",   zip: 38570,   email:"bujapve@neucaro.me",  teacher: "RAJ SINGH" },
+    
+   ]
+}
